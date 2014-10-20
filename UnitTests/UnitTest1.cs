37c568d0af59ff6076795bc37135f176de16fc76
Bradley Cunningham <bradley.cunningham@gmail.com>
2014-10-20 11:51:07 +0200

    Initial Commit

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Domain.Abstract;
+using Domain.Entities;
+using WebUI.Controllers;
+using System.Collections.Generic;
+
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Can_Paginate()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {new Product{ ProductID = 1, Name = "P1" }, new Product { ProductID = 2, Name = "P2" }, new Product { ProductID = 3, Name = "P3" }, new Product { ProductID = 4, Name = "P4" }, new Product { ProductID = 5, Name = "P5" }});
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+            IList<Product> ps = new List<Product>();
+
+            foreach (Product p in result)
+            {
+                ps.Add(p);
+            }
+
+            Assert.IsTrue(ps.Count == 2);
+            Assert.AreEqual(ps[0].Name, "P4");
+            Assert.AreEqual(ps[1].Name, "P5");
+        }
+    }
+}
---
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Abstract;
+using Domain.Concrete;
+using Domain.Entities;
+using Moq;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver: IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            //mock.Setup(m => m.Products).Returns(new List<Product> { new Product{Name = "Football", Price = 25 }, new Product{Name = "Surf Board", Price = 179}, new Product{Name="Running Shoes"}});
+
+            //kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+    }
+}
