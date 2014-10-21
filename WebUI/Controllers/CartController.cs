6c0140ae2525f0edeb30b22ffb52d7ed9571ed21
Bradley Cunningham <bradley.cunningham@gmail.com>
2014-10-21 19:15:15 +0200

    Including Excluded Files

---
+++ WebUI/Controllers/CartController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Abstract;
+using Domain.Entities;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+        private IOrderProcessor orderProcessor;
+
+        public CartController(IProductRepository repository, IOrderProcessor proc)
+        {
+            this.repository = repository;
+            this.orderProcessor = proc;
+        }
+
+        public ViewResult Index(Cart cart, string returnUrl)
+        {
+            return View(new CartIndexViewModel { Cart = cart, ReturnUrl = returnUrl });
+        }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
+
+        [HttpPost]
+        public ViewResult Checkout(Cart cart, ShippingDetails shipingDetails)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry your cart is empty");
+            }
+
+            if (ModelState.IsValid)
+            {
+                orderProcessor.ProcessOrder(cart, shipingDetails);
+                cart.Clear();
+                return View("Completed");
+            }
+            else
+            {
+                return View("shippingDetails");
+            }
+        }
+
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+
+        public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            if(product != null)
+            {
+                cart.AddItem(product, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            if (product != null)
+            {
+                cart.RemoveLine(product);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        //private Cart GetCart()
+        //{
+        //    Cart cart = (Cart)Session["Cart"];
+        //    if (cart == null)
+        //    {
+        //        cart = new Cart();
+        //        Session["Cart"] = cart;
+        //    }
+
+        //    return cart;
+        //}
+	}
+}
